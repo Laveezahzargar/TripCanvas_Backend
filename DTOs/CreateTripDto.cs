3ba6e3237e51805e1e3e9742a20b32e2dce25b4e
laveezahzargar <laveezahzargar@gmail.com>
2026-06-10 16:55:00 +0530

    updates

---
+++ DTOs/CreateTripDto.cs
@@ -0,0 +1,10 @@
+﻿namespace P6_Travel_Planner_Backend.DTOs
+{
+    public class CreateTripDto
+    {
+        public string Title { get; set; }
+        public int DestinationId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ DTOs/UpdateTripDto.cs
@@ -0,0 +1,12 @@
+﻿using P6_Travel_Planner_Backend.Enums;
+
+namespace P6_Travel_Planner_Backend.DTOs
+{
+    public class UpdateTripDto
+    {
+        public string Title { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public TripStatus Status { get; set; }
+    }
+}
